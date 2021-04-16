a3aa176f907e6988a5535c57a67c8d08cab23dbc
Raymond Lee <raymanl33@icloud.com>
2021-04-15 19:02:34 -0700

    having fun?

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,56 @@
 
+function defineBudgetArea() {
+    let budget = document.querySelector('#CreateBudget')
 
-
-function storebudget() {
-    const budget = document.querySelector('#CreateBudget')
-    let currentbudget = budget.value
-    console.log(currentbudget)
-    return currentbudget
+    html = `
+    <div id="budgetarea"> 
+    <h2>My Budget: ${budget.value}</h2>
+    <p>current remaining budget: ${budget.value}</p> 
+    `
+    return html
 }
 
-function calculation() {
-    console.log(storebudget())
+function defineExpenseArea() {
+    let item = document.querySelector('#ExpItem')
+    let amount = document.querySelector('#ExpAmount')
+    html = `
+    <div class="AddExpense">
+        <ul>
+            <li>${item.value} ${amount.value}</li>
+        </ul>
+    </div>`
+
+    return html
+}
+
+function addExpense() {
+    let form = document.querySelector('#budgetarea')
+    form.insertAdjacentHTML('beforeend', defineExpenseArea())
+    calculateExpense()
+}
+
+
+function addEventlistner() {
+    let form = document.querySelector('.form')
+    form.addEventListener('click', (e) => {
+        console.log(e.target.innerText)
+    })
+}
+
+function storebudget() {
+    let form = document.querySelector('.form')
+    addEventlistner()
+    form.insertAdjacentHTML('afterend', defineBudgetArea())
+ 
+
+}
+
+function calculateExpense() {
+    let amount = document.querySelector('#ExpAmount')
+    let budget = document.querySelector('#CreateBudget')
+    const updateBudget = document.querySelector('p')
+    let total = budget.value - (amount.value)
+    console.log(total)
+  
+    updateBudget.innerText =  'current remaing budget:' + total;
 }
